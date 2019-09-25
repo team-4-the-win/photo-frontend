8f1e324f4060badd815ef13e59a24cff1c217eb2
sookim-Boston <soojeong914@gmail.com>
2019-09-25 16:54:56 -0400

    Add auth folder
    Add api.js, events.js, ui.js

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const signUpSuccess = function () {
+}
+
+const signUpFailure = function () {
+}
+
+const signInSuccess = function (data) {
+}
+
+const signInFailure = function () {
+}
+
+const changePasswordSuccess = function (data) {
+}
+
+const changePasswordFailure = function (data) {
+}
+
+const signOutSuccess = function () {
+}
+
+const signOutFailure = function () {
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
