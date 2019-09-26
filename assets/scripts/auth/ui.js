27590872084bcfe20f37f5e07083d2a0935e2fd4
sookim-Boston <soojeong914@gmail.com>
2019-09-26 09:27:19 -0400

    Add ui.js to auth

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,6 +2,7 @@
 
 const signUpSuccess = function () {
 <<<<<<< HEAD
+<<<<<<< HEAD
   $('#sign-up-message').text('Signed up successfully')
 }
 
@@ -36,28 +37,45 @@ const signOutSuccess = function () {
 const signOutFailure = function () {
   $('#sign-out-message').text('Error occured.')
 =======
+=======
+  $('#sign-up-message').text('Signed up successfully')
+>>>>>>> Add feedback for user (auth)
 }
 
 const signUpFailure = function () {
+  $('#sign-up-message').text('Error occured.')
 }
 
-const signInSuccess = function (data) {
+const signInSuccess = function () {
+  $('#sign-in-message').text('Signed in successfully')
+  $('#sign-in-div').hide()
+  $('#sign-up-div').hide()
 }
 
 const signInFailure = function () {
+  $('#sign-in-message').text('Error occured.')
 }
 
-const changePasswordSuccess = function (data) {
+const changePasswordSuccess = function () {
+  $('#change-pw-message').text('Changed password successfully')
 }
 
-const changePasswordFailure = function (data) {
+const changePasswordFailure = function () {
+  $('#change-pw-message').text('Error occured.')
 }
 
 const signOutSuccess = function () {
+  $('#sign-out-message').text('Signed out successfully')
+  $('#sign-in-div').show()
+  $('#sign-up-div').show()
 }
 
 const signOutFailure = function () {
+<<<<<<< HEAD
 >>>>>>> Add auth folder
+=======
+  $('#sign-out-message').text('Error occured.')
+>>>>>>> Add feedback for user (auth)
 }
 
 module.exports = {
