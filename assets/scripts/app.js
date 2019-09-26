2689373899ea5613abcfdd566f24d70699135651
D-Raimon <raimon67@gmail.com>
2019-09-26 11:19:19 -0400

    Fix merge conflict

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,17 +8,13 @@ const authEvents = require('./auth/events.js')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-const authEvents = require('./auth/events')
 
 $(() => {
   imageUploadEvents.addHandlers()
-<<<<<<< HEAD
   // your JS code goes here
   $('#sign-up-form').on('submit', authEvents.onSignUp)
   $('#sign-in-form').on('submit', authEvents.onSignIn)
   $('#reset-pw-form').on('submit', authEvents.onChangePassword)
   $('#sign-out-button').on('click', authEvents.onSignOut)
-=======
   authEvents.addHandlers()
->>>>>>> Make change to auth and change to app.js
 })
