19144ce4909b43c19e014a96a73c8f065f20624d
jennygodley <jennifergodley@gmail.com>
2019-09-27 15:32:10 -0400

    Add update/delete modal and update styling
    Updated styling with @sarahzawatsky - changed colors of page and
    updated bootstrap colors.
    
    Adding in @sookim914's updates to add the update/delete modal as well.

--- assets/scripts/image-upload/ui.js
+++ assets/scripts/image-upload/ui.js
@@ -1,12 +1,16 @@
 'use strict'
 
 const store = require('../store')
+const events = require('./events')
 const showImagesTemplate = require('../templates/imageUpload-listing.handlebars')
 
 // UPON SUCCESSFUL IMAGE UPLOAD
 const onUploadSuccess = function (data) {
   $('.display').html('<img class="img-fluid" src="' + data.fileUpload.url + '">')
   $('#create-files-message').text('File has successfully uploaded')
+  store.file = data.fileUpload
+  // onGetUploadsSuccess()
+  // console.log(store.file)
 }
 
 // UPON SUCCESSFUL GET IMAGES
@@ -15,7 +19,17 @@ const onGetUploadsSuccess = function (data) { // added data as a parameter
     return image.user === store.user._id
   })
   $('#get-files-message').text('Take a look around!')
-  // new shit under here
+  const showImagesHtml = showImagesTemplate({ images: myImages })
+  $('.get-files').html(showImagesHtml)
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+}
+
+const onGetBrowseSuccess = function (data) { // added data as a parameter
+  const myImages = data.fileUploads.filter(function (image) {
+    return image.user !== store.user._id
+  })
   const showImagesHtml = showImagesTemplate({ images: myImages })
   $('.get-files').html(showImagesHtml)
   $(function () {
@@ -25,5 +39,6 @@ const onGetUploadsSuccess = function (data) { // added data as a parameter
 
 module.exports = {
   onUploadSuccess,
-  onGetUploadsSuccess
+  onGetUploadsSuccess,
+  onGetBrowseSuccess
 }
