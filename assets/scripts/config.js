5417e1382dda1d9c450ab58e4342963b3cb3dda0
jennygodley <jennifergodley@gmail.com>
2019-09-27 11:55:35 -0400

    Update styling
    Updated styling. Integrating changes as well from @sarahzawatsky,
    @sookim914, and @D-Raimon.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'http://blooming-cliffs-96004.herokuapp.com',
+  production: 'https://blooming-cliffs-96004.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
